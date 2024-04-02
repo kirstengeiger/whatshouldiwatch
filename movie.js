7f28fcfd2132448a62c4c18492d359f0c01362c8
kirstengeiger <geigk206@newschool.edu>
2024-04-02 00:09:42 -0400

    Now I properly added the JSON

--- movie.js
+++
@@ -1,9 +0,0 @@
-// const apiKey = "c43b7cbd";
-fetch(`http://www.omdbapi.com/?apikey=c43b7cbd&`)
-    .then(res => res.json())
-    .then(data => console.log(data))
-
-// console.log(fetch)
-console.log('hello')
-
-  
