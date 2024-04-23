790fcfea7389752b775231d231e79f556c8c22e4
kirstengeiger <geigk206@newschool.edu>
2024-04-23 11:37:12 -0400

    Connected .suggest-movie button in mobile and desktop

--- assets/main.js
+++ assets/main.js
@@ -28,153 +28,159 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 
-    // Add click event listener to the "Suggest Movie" button
-    document.getElementById('suggest-button').addEventListener('click', () => {
-        const selectedYears = Array.from(document.querySelectorAll('.year-checkbox:checked')).map(checkbox => checkbox.value);
-        
-        fetch('assets/data.json')
-            .then(response => response.json())
-            .then(data => {
-                let filteredData = data;
+    // Add event listener to the "Suggest Movie" button in intro-mobile
+    const suggestButtonMobile = document.querySelector('.intro-mobile .suggest-movie button#suggest-button');
+    suggestButtonMobile.addEventListener('click', suggestMovie);
 
-                // Filter based on selected years
-                if (selectedYears.length > 0) {
-                    filteredData = data.filter(item => {
-                        for (const yearRange of selectedYears) {
-                            const [startYear, endYear] = yearRange.split('-').map(Number);
-                            if (item.year >= startYear && item.year <= endYear) {
-                                return true;
-                            }
-                        }
-                        return false;
-                    });
-                }
-
-                if (filteredData.length > 0) {
-                    const randomIndex = Math.floor(Math.random() * filteredData.length);
-                    const randomEntryData = filteredData[randomIndex];
-
-                    const randomEntryHTML = `
-                        <li class="suggestion-mobile">
-                            <section class="top-bar">
-                                <button class="exit-button">X</button>
-                                <button class="refresh-button"><img src="images/refreshbutton.png"></button>
-                            </section>
-                            <section class="movie-info">
-                                <h1 class="title">${randomEntryData.title}</h1>
-                                <hr>
-                                <section class="movie-title-poster">
-                                    <img class="poster" src="${randomEntryData.poster}">
-                                </section>
-                                <hr>
-                                <section class="movie-statistics">
-                                        <p class="imdbRating">⭑ ${randomEntryData.imdbRating}</p>
-                                        <p class="genre">${randomEntryData.genre}</p>
-                                        <p class="yearReleased"><time>${randomEntryData.year}</time></p>
-                                        <p class="runTime"><em>${randomEntryData.runTime}</em></p>
-                                </section>
-                                <hr>
-                                <p class="plot">${randomEntryData.plot}</p>
-                                <hr>
-                                <section class="movie-people">
-                                    <p class="director">Directed by ${randomEntryData.director}</p>
-                                    <p class="starring">Starring ${randomEntryData.stars}</p>
-                                </section>
-                                <hr>
-                                <p class="streaming"><a href="${randomEntryData.streamLink}">Watch now</a></p>
-                            </section>
-                        </li>
-
-                        <li class="suggestion-tablet">
-                            <section class="movie-info">
-                                <section class="movie-top">
-                                    <h1 class="title">${randomEntryData.title}</h1>
-                                </section>
-                                <section class="movie-below">
-                                    <section class="movie-left">
-                                        <img class="poster" src="${randomEntryData.poster}">
-                                    </section>
-                                    <section class="movie-right">
-                                        <section class="movie-statistics">
-                                                <p class="imdbRating">⭑ ${randomEntryData.imdbRating}</p>
-                                                <p class="genre">${randomEntryData.genre}</p>
-                                                <p class="yearReleased"><time>${randomEntryData.year}</time></p>
-                                                <p class="runTime"><em>${randomEntryData.runTime}</em></p>
-                                        </section>
-                                        <hr>
-                                        <p class="plot">${randomEntryData.plot}</p>
-                                        <hr>
-                                        <section class="movie-people">
-                                            <p class="director">Directed by ${randomEntryData.director}</p>
-                                            <p class="starring">Starring ${randomEntryData.stars}</p>
-                                        </section>
-                                        <hr>
-                                        <p class="streaming">Streaming on ${randomEntryData.streamPlatform} <a href="${randomEntryData.streamLink}">linked here</a></p>
-                                    </section>
-                                </section>
-                            </section
-                        </li>
-
-                        <li class="suggestion-desktop">
-                            <section class="movie-info">
-                                <section class="movie-below">
-                                    <section class="movie-left">
-                                        <img class="poster" src="${randomEntryData.poster}">
-                                    </section>
-                                    <section class="movie-right">
-                                        <h1 class="title">${randomEntryData.title}</h1>
-                                        <section class="movie-statistics">
-                                                <p class="imdbRating">⭑ ${randomEntryData.imdbRating}</p>
-                                                <p class="genre">${randomEntryData.genre}</p>
-                                                <p class="yearReleased"><time>${randomEntryData.year}</time></p>
-                                                <p class="runTime"><em>${randomEntryData.runTime}</em></p>
-                                        </section>
-                                        <p class="plot">${randomEntryData.plot}</p>
-                                        <section class="movie-people">
-                                            <p class="director">Directed by ${randomEntryData.director}</p>
-                                            <p class="starring">Starring ${randomEntryData.stars}</p>
-                                        </section>
-                                        <a href="${randomEntryData.streamLink}"><p class="streaming">Streaming on ${randomEntryData.streamPlatform}</p></a>
-                                    </section>
-                                </section>
-                            </section
-                        </li>
-                    `;
-
-                    const dataList = document.getElementById('suggested-movie');
-                    dataList.innerHTML = '';
-                    dataList.insertAdjacentHTML('beforeend', randomEntryHTML);
-
-                    console.log('Random Entry:', randomEntryData);
-                } else {
-                    console.log('No movies found for the selected years.');
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-    });
-
-    // Function to handle click on exit button
-    const handleExitButtonClick = (event) => {
-        const exitButton = event.target.closest('.exit-button');
-        if (exitButton) {
-            const listItem = exitButton.closest('.suggestion-mobile');
-            if (listItem) {
-                listItem.remove();
-            }
-        }
-    };
-
-    // Add click event listener to the document and delegate it to exit buttons
-    document.addEventListener('click', handleExitButtonClick);
-
-    // Add event listener to toggle movie section visibility
-    const suggestButton = document.getElementById('suggest-button');
-    const movieSection = document.querySelector('.movie');
-
-    suggestButton.addEventListener('click', function() {
-        movieSection.classList.add('active'); // Add the "active" class to show the movie section
-    });
+    // Add event listener to the "Suggest Movie" button in intro-desktop
+    const suggestButtonDesktop = document.querySelector('.intro-desktop .suggest-movie button#suggest-button');
+    suggestButtonDesktop.addEventListener('click', suggestMovie);
 });
 
+// Define the suggestMovie function to handle the "Suggest Movie" button click
+function suggestMovie() {
+    // Retrieve selected years
+    const selectedYears = Array.from(document.querySelectorAll('.year-checkbox:checked')).map(checkbox => checkbox.value);
+    fetch('assets/data.json')
+        .then(response => response.json())
+        .then(data => {
+            let filteredData = data;
+
+            // Filter based on selected years
+            if (selectedYears.length > 0) {
+                filteredData = data.filter(item => {
+                    for (const yearRange of selectedYears) {
+                        const [startYear, endYear] = yearRange.split('-').map(Number);
+                        if (item.year >= startYear && item.year <= endYear) {
+                            return true;
+                        }
+                    }
+                    return false;
+                });
+            }
+
+            if (filteredData.length > 0) {
+                const randomIndex = Math.floor(Math.random() * filteredData.length);
+                const randomEntryData = filteredData[randomIndex];
+
+                const randomEntryHTML = `
+                <li class="suggestion-mobile">
+                <section class="top-bar">
+                    <button class="exit-button">X</button>
+                    <button class="refresh-button"><img src="images/refreshbutton.png"></button>
+                </section>
+                <section class="movie-info">
+                    <h1 class="title">${randomEntryData.title}</h1>
+                    <hr>
+                    <section class="movie-title-poster">
+                        <img class="poster" src="${randomEntryData.poster}">
+                    </section>
+                    <hr>
+                    <section class="movie-statistics">
+                            <p class="imdbRating">⭑ ${randomEntryData.imdbRating}</p>
+                            <p class="genre">${randomEntryData.genre}</p>
+                            <p class="yearReleased"><time>${randomEntryData.year}</time></p>
+                            <p class="runTime"><em>${randomEntryData.runTime}</em></p>
+                    </section>
+                    <hr>
+                    <p class="plot">${randomEntryData.plot}</p>
+                    <hr>
+                    <section class="movie-people">
+                        <p class="director">Directed by ${randomEntryData.director}</p>
+                        <p class="starring">Starring ${randomEntryData.stars}</p>
+                    </section>
+                    <hr>
+                    <p class="streaming"><a href="${randomEntryData.streamLink}">Watch now</a></p>
+                </section>
+            </li>
+
+            <li class="suggestion-tablet">
+                <section class="movie-info">
+                    <section class="movie-top">
+                        <h1 class="title">${randomEntryData.title}</h1>
+                    </section>
+                    <section class="movie-below">
+                        <section class="movie-left">
+                            <img class="poster" src="${randomEntryData.poster}">
+                        </section>
+                        <section class="movie-right">
+                            <section class="movie-statistics">
+                                    <p class="imdbRating">⭑ ${randomEntryData.imdbRating}</p>
+                                    <p class="genre">${randomEntryData.genre}</p>
+                                    <p class="yearReleased"><time>${randomEntryData.year}</time></p>
+                                    <p class="runTime"><em>${randomEntryData.runTime}</em></p>
+                            </section>
+                            <hr>
+                            <p class="plot">${randomEntryData.plot}</p>
+                            <hr>
+                            <section class="movie-people">
+                                <p class="director">Directed by ${randomEntryData.director}</p>
+                                <p class="starring">Starring ${randomEntryData.stars}</p>
+                            </section>
+                            <hr>
+                            <p class="streaming">Streaming on ${randomEntryData.streamPlatform} <a href="${randomEntryData.streamLink}">linked here</a></p>
+                        </section>
+                    </section>
+                </section
+            </li>
+
+            <li class="suggestion-desktop">
+                <section class="movie-info">
+                    <section class="movie-below">
+                        <section class="movie-left">
+                            <img class="poster" src="${randomEntryData.poster}">
+                        </section>
+                        <section class="movie-right">
+                            <h1 class="title">${randomEntryData.title}</h1>
+                            <section class="movie-statistics">
+                                    <p class="imdbRating">⭑ ${randomEntryData.imdbRating}</p>
+                                    <p class="genre">${randomEntryData.genre}</p>
+                                    <p class="yearReleased"><time>${randomEntryData.year}</time></p>
+                                    <p class="runTime"><em>${randomEntryData.runTime}</em></p>
+                            </section>
+                            <p class="plot">${randomEntryData.plot}</p>
+                            <section class="movie-people">
+                                <p class="director">Directed by ${randomEntryData.director}</p>
+                                <p class="starring">Starring ${randomEntryData.stars}</p>
+                            </section>
+                            <a href="${randomEntryData.streamLink}"><p class="streaming">Streaming on ${randomEntryData.streamPlatform}</p></a>
+                        </section>
+                    </section>
+                </section
+            </li>
+                `;
+
+                const dataList = document.getElementById('suggested-movie');
+                dataList.innerHTML = '';
+                dataList.insertAdjacentHTML('beforeend', randomEntryHTML);
+
+                console.log('Random Entry:', randomEntryData);
+            } else {
+                console.log('No movies found for the selected years.');
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+// Function to handle click on exit button
+function handleExitButtonClick(event) {
+    const exitButton = event.target.closest('.exit-button');
+    if (exitButton) {
+        const listItem = exitButton.closest('.suggestion-mobile');
+        if (listItem) {
+            listItem.remove();
+        }
+    }
+}
+
+// Add click event listener to the document and delegate it to exit buttons
+document.addEventListener('click', handleExitButtonClick);
+
+// Add event listener to toggle movie section visibility
+const suggestButton = document.getElementById('suggest-button');
+const movieSection = document.querySelector('.movie');
+suggestButton.addEventListener('click', function() {
+    movieSection.classList.add('active'); // Add the "active" class to show the movie section
+});
