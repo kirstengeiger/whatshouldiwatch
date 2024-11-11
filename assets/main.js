967ca3ca5bebae2b880c818df53966634471f7e9
kirstengeiger <geigk206@newschool.edu>
2024-11-11 01:22:59 -0500

    Added instructions and text

--- assets/main.js
+++ assets/main.js
@@ -1,93 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Select the label elements in both desktop and mobile versions
-    const labels = document.querySelectorAll('.intro-desktop .filter-year .label, .intro-mobile .filter-year .label');
-
-    // Add click event listeners to all label elements
-    labels.forEach(label => {
-        label.addEventListener('click', function() {
-            // Toggle the 'active' class on the parent <ul> element
-            label.parentElement.classList.toggle('active');
-        });
-    });
-
-    // Add change event listener to checkboxes
-    document.querySelectorAll('.year-checkbox').forEach(checkbox => {
-        checkbox.addEventListener('change', () => {
-            const selectedYears = Array.from(document.querySelectorAll('.year-checkbox:checked')).map(checkbox => checkbox.value);
-            console.log(selectedYears);
-        });
-    });
-
-    // Add event listener to the "Suggest Movie" buttons in both mobile and desktop versions
+    // Select the "Suggest Movie" buttons in both mobile and desktop versions
     const suggestButtonMobile = document.getElementById('suggest-button-mobile');
-    suggestButtonMobile.addEventListener('click', suggestMovie);
-
     const suggestButtonDesktop = document.getElementById('suggest-button-desktop');
+
+    suggestButtonMobile.addEventListener('click', suggestMovie);
     suggestButtonDesktop.addEventListener('click', suggestMovie);
 });
 
 // Define the suggestMovie function to handle the "Suggest Movie" button click
 function suggestMovie() {
-    // Retrieve selected years
-    const selectedYears = Array.from(document.querySelectorAll('.year-checkbox:checked')).map(checkbox => checkbox.value);
     fetch('assets/data.json')
         .then(response => response.json())
         .then(data => {
-            let filteredData = data;
+            // Select a random movie from the data
+            const randomIndex = Math.floor(Math.random() * data.length);
+            const randomEntryData = data[randomIndex];
 
-            // Filter based on selected years
-            if (selectedYears.length > 0) {
-                filteredData = data.filter(item => {
-                    for (const yearRange of selectedYears) {
-                        const [startYear, endYear] = yearRange.split('-').map(Number);
-                        if (item.year >= startYear && item.year <= endYear) {
-                            return true;
-                        }
-                    }
-                    return false;
-                });
-            }
-
-            if (filteredData.length > 0) {
-                const randomIndex = Math.floor(Math.random() * filteredData.length);
-                const randomEntryData = filteredData[randomIndex];
-
-                const randomEntryHTML = `
-                    <li class="suggestion-mobile">
-                        <section class="top-bar">
-                            <button class="exit-button">X</button>
+            const randomEntryHTML = `
+                <li class="suggestion-mobile">
+                    <section class="movie-info">
+                        <section class="movie-top">
+                            <section class="movie-poster">
+                                <img class="poster" src="${randomEntryData.poster}">
+                            </section>
                         </section>
-                        <section class="movie-info">
-                            <section class="movie-top">
-                                <section class="movie-poster">
-                                    <img class="poster" src="${randomEntryData.poster}">
-                                </section>
+                        <hr>
+                        <section class="movie-bottom">
+                            <h3>${randomEntryData.title}</h3>
+                            <hr>
+                            <section class="movie-statistics">
+                                    <p class="imdbRating">⭑ ${randomEntryData.imdbRating}</p>
+                                    <p class="genre">${randomEntryData.genre}</p>
+                                    <p class="yearReleased"><time>${randomEntryData.year}</time></p>
+                                    <p class="runTime"><em>${randomEntryData.runTime}</em></p>
                             </section>
                             <hr>
-                            <section class="movie-bottom">
-                                <h1 class="title">${randomEntryData.title}</h1>
-                                <hr>
-                                <section class="movie-statistics">
-                                        <p class="imdbRating">⭑ ${randomEntryData.imdbRating}</p>
-                                        <p class="genre">${randomEntryData.genre}</p>
-                                        <p class="yearReleased"><time>${randomEntryData.year}</time></p>
-                                        <p class="runTime"><em>${randomEntryData.runTime}</em></p>
-                                </section>
-                                <hr>
-                                <p class="plot">${randomEntryData.plot}</p>
-                                <hr>
-                                <section class="movie-people">
-                                    <p class="director">Directed by ${randomEntryData.director}</p>
-                                    <p class="starring">Starring ${randomEntryData.stars}</p>
-                                </section>
-                                <hr>
-                            </section>
-                        <section class="stream-link">
-                            <p class="streaming"><a href="${randomEntryData.streamLink}">▶</a></p>
+                            <p class="plot">${randomEntryData.plot}</p>
+                            <hr>
+                            <hr>
                         </section>
-                    </li>
+                    <section class="stream-link">
+                        <p class="streaming"><a href="${randomEntryData.streamLink}">▶</a></p>
+                    </section>
+                </li>
 
-                    <li class="suggestion-desktop">
+                <li class="suggestion-desktop">
                     <section class="movie-info">
                         <section class="movie-below">
                             <section class="movie-left">
@@ -111,25 +68,20 @@ function suggestMovie() {
                                 </section>
                             </section>
                             <a href="${randomEntryData.streamLink}">
-                            <section class="streaming-link">
-                                <p class="streaming">Streaming on ${randomEntryData.streamPlatform}</p>
-                            </section>
-                        </a>
+                                <section class="streaming-link">
+                                    <p class="streaming">Streaming on ${randomEntryData.streamPlatform}</p>
+                                </section>
+                            </a>
                         </section>
-                    </section
+                    </section>
                 </li>
+            `;
 
-                
-                `;
+            const dataList = document.getElementById('suggested-movie');
+            dataList.innerHTML = '';
+            dataList.insertAdjacentHTML('beforeend', randomEntryHTML);
 
-                const dataList = document.getElementById('suggested-movie');
-                dataList.innerHTML = '';
-                dataList.insertAdjacentHTML('beforeend', randomEntryHTML);
-
-                console.log('Random Entry:', randomEntryData);
-            } else {
-                console.log('No movies found for the selected years.');
-            }
+            console.log('Random Entry:', randomEntryData);
         })
         .catch(error => {
             console.error('Error fetching data:', error);
